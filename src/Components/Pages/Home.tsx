e7b3d009be8f4e5db4dfbaa4642ebf6009674bcf
Shobhan Karthick <loganshobhanwolverine@gmail.com>
2021-09-24 10:46:20 +0530

    initial commit

---
+++ src/Components/Pages/Home.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+interface Props {
+    
+}
+
+const Home = (props: Props) => {
+    return (
+        <div>
+          <h1>Welcome to Sampark 2022!</h1>
+        </div>
+    )
+}
+
+export default Home
+
+
