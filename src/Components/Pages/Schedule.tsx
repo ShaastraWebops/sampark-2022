fec2387122e0f13663fcd7b8e70ee426a4a9d18d
JanithMS <janithms9920@gmail.com>
2021-10-15 12:10:27 +0530

    changed schedule page UI

--- src/Components/Pages/Schedule.tsx
+++ src/Components/Pages/Schedule.tsx
@@ -11,30 +11,31 @@ const Schedule = (props: Props) => {
     <div className="schedule">
       <Navbar />
       <Title title="SCHEDULE" isHomePage={false} />
-      <div className="schedule-content">
-        {schedules.map((schedule, index) => (
-          <div>
-            <div className="workshop-day">
-              <div className="workshop-day-day">{schedule.day}</div>
-              <div className="workshop-day-day">-</div>
-              <div className="workshop-day-date">{schedule.date}</div>
-            </div>
-            <div>
-              {schedule.schedule.map((workshop) => {
-                return workshop.isBreak ? (
-                  <div className="workshop-break">{"BREAK (" + workshop.time + ")"}</div>
-                ) : (
-                  <div className="workshop-schedule">
-                    <div className="workshop-schedule-name">{workshop.name}</div>
-                    <div className="workshop-schedule-time">{workshop.time?.toLocaleUpperCase()}</div>
-                  </div>
-                );
-              })}
-            </div>
-            {index !== (schedules.length - 1) && <div className="day-end-line"></div>}
+      {schedules.map((schedule, index) => (
+        <div className="schedule-content">
+          <div className="workshop-day">
+            <div className="workshop-day-day">{schedule.day}</div>
+            <div className="workshop-day-day">-</div>
+            <div className="workshop-day-date">{schedule.date}</div>
           </div>
-        ))}
-      </div>
+          <div>
+            {schedule.schedule.map((workshop) => {
+              return workshop.isBreak ? (
+                <div className="workshop-break">
+                  {"BREAK (" + workshop.time + ")"}
+                </div>
+              ) : (
+                <div className="workshop-schedule">
+                  <div className="workshop-schedule-name">{workshop.name}</div>
+                  <div className="workshop-schedule-time">
+                    {workshop.time?.toLocaleUpperCase()}
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      ))}
     </div>
   );
 };
